0a4f6ba80ffb16c49b45812d44929e1c7c90b440
Saher Elgendy <dr.codergen@gmail.com>
2020-10-06 10:12:23 -0700

    testing App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function App({text}) {
   return (
     <div className="App">
-     
+     <header data-testid="header">{text}</header>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+import '@testing-library/jest-dom/extend-expect'
 
+describe('it should', () => {
+  test('render App correctly', () => {
+    const { container, getByText, getByTestId } = render(<App text="React Testing"></App>);
+    //creating snapshot
+    expect(container.firstChild).toMatchSnapshot();
+    //check if header exist in the document
+    const header = getByTestId(/header/);
+    expect(header).toHaveTextContent('React Testing');
+  })
+})
