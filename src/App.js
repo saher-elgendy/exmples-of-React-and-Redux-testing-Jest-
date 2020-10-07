5b53963071b24b570038012eb5c44d4da79300e2
Saher Elgendy <dr.codergen@gmail.com>
2020-10-07 07:27:48 -0700

    creating App component and getting data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { connect } from 'react-redux'
+import { fetchData } from './redux/actions';
+import BookShelf from './components/bookShelf';
+import Bag from './components/bag';
 
-function App({text}) {
+
+function App({ fetchData, data: { loading, errors, books } }) {
+  useEffect(() => {
+    fetchData()
+  }, []);
+
   return (
     <div className="App">
-     <header data-testid="header">{text}</header>
+      <header data-testid="header">
+        <h2>React Testing</h2>
+        <Bag />
+      </header>
+      {
+        !loading ? <BookShelf books={books} /> : '...loading books'
+      }
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  data: state.data
+});
+
+export default connect(mapStateToProps, { fetchData })(App);
