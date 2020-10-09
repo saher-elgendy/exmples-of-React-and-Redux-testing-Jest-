04b2e8ff409e9f6cb47a4845362150b7dbce535b
Saher Elgendy <dr.codergen@gmail.com>
2020-10-09 10:18:28 -0700

    add bag action creators

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,4 +1,10 @@
+
+//data
 export const FETCH_DATA = 'FETCH_DATA'; 
 export const SET_LOADING = 'SET_LOADING';
 export const STOP_LOADING = 'STOP_LOADING';
-export const SET_ERRORS  = 'SET_ERRORS';
+export const SET_ERROR = 'SET_ERRORS';
+
+//bag
+export const ADD_BOOK = 'ADD_BOOK';
+export const REMOVE_BOOK = 'REMOVE_BOOK';
