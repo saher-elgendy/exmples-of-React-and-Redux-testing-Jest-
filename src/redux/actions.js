9a839bbde6669f024753f6a314c46e17cd147c86
Saher Elgendy <dr.codergen@gmail.com>
2020-10-09 10:18:58 -0700

    add bag action creators

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,16 @@
-import { FETCH_DATA,  SET_ERRORS, SET_LOADING, STOP_LOADING } from './actionTypes';
+import {
+    FETCH_DATA,
+    SET_ERROR,
+    SET_LOADING,
+    STOP_LOADING,
+    ADD_BOOK,
+    REMOVE_BOOK
+} from './actionTypes';
 import axios from 'axios';
 
+
 export const fetchData = () => dispatch => {
-    dispatch({type: SET_LOADING});
+    dispatch({ type: SET_LOADING });
 
     return axios.get("https://api.jsonbin.io/b/5f7d3ff8302a837e95760fea")
         .then(res => {
@@ -10,12 +18,39 @@ export const fetchData = () => dispatch => {
                 type: FETCH_DATA,
                 payload: res.data.books
             });
-            dispatch({type: STOP_LOADING})
+            dispatch({ type: STOP_LOADING })
         })
-        .catch(err =>{
+        .catch(err => {
             dispatch({
-                type: SET_ERRORS,
-                payload: 'No Data found'
+                type: SET_ERROR,
+                payload: 'Something went wrong'
             })
         })
 }
+
+
+/*bag */
+
+export const addBookToBag = (book) => (dispatch, getState) => {
+    const bag = getState().bag;
+    const newBag = [...bag];
+    const index = newBag.findIndex(b => b.cover === book.cover);
+
+    if (index < 0) {
+        return dispatch({
+            type: ADD_BOOK,
+            payload: [...newBag, book]
+        })
+    }
+}
+
+export const removeBookFromBag = book => (dispatch, getState) => {
+    const bag = getState().bag;
+    const newBag = [...bag];
+
+    return dispatch({
+        type: REMOVE_BOOK,
+        payload: newBag.filter(b => b.cover !== book.cover)
+    })
+}
+
