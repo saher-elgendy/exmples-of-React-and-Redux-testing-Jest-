6a02e621b694400b4b1e2c08c33bdbd7e87638ce
Saher Elgendy <dr.codergen@gmail.com>
2020-10-09 10:17:13 -0700

    creating Bag component

--- src/components/bag.js
+++ src/components/bag.js
@@ -1,16 +1,68 @@
-import React from 'react';
-import bagIcon from './../static/icon.png'
+import React, { useState, useEffect } from 'react';
+import bagIcon from './../static/icon.png';
+import { connect } from 'react-redux';
 
-const Bag = () => {
+
+const Bag = ({ bag }) => {
+    const [open, setOpen] = useState(false);
+
+    const toggleOpenBag = () => {
+        setOpen(!open)
+    }
+
+    useEffect(() => {
+        if (open) {
+            document.addEventListener('click', toggleOpenBag);
+        }
+
+        return () => {
+            document.removeEventListener('click', toggleOpenBag)
+        }
+    }, [open]);
+
     return (
         <div className="bag">
             <div className="bag-container">
-                <span className="book-count">3</span>
-                <img src={bagIcon} alt="bag" />
-            </div>
+                <span className="book-count">{bag.length}</span>
+                <img src={bagIcon} alt="bag" onClick={toggleOpenBag} />
 
+                {
+                    open && <div className="bag-content">
+                        <ul className="bag-books">
+                            {
+                                bag.map((book, i) => {
+                                    return (
+                                        <>
+                                            <li key={i} className="bag-item">
+                                                <img src={book.cover} alt="book_cover" className="bag-book-cover" />
+                                                <div className="book-details">
+                                                    <span style={{ textAlign: 'left' }}>{book.name}</span>
+                                                    <span style={{
+                                                        fontSize: '0.8rem',
+                                                        color: 'rgb(167 167 167)',
+                                                        display: 'block'
+                                                    }}>
+                                                        {book.author}</span>
+                                                </div>
+
+                                            </li>
+
+                                            {i !== bag.length - 1 && <hr />}
+                                        </>
+                                    )
+
+                                })
+                            }
+                        </ul>
+                    </div>
+                }
+            </div>
         </div>
     )
 }
 
-export default Bag;
+const mapStateToProps = state => ({
+    bag: state.bag
+});
+
+export default connect(mapStateToProps)(Bag);
